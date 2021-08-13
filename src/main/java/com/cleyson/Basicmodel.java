6ea6b00a4a5313ba5540271cf930de00c5589905
jobcfb <jobcfb@gmail.com>
2021-08-13 11:05:32 +0100

    First commit

---
+++ src/main/java/com/cleyson/Basicmodel.java
@@ -0,0 +1,11 @@
+package com.cleyson;
+
+public class Basicmodel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello basics");
+
+	}
+
+}
